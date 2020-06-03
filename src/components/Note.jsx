bd8167e98c9e8d817fa7689e67d3ff76b81b867b
Asmita Pathare <asmitapat9@gmail.com>
2020-06-03 14:18:24 +0530

    ES6 Arrow Function

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-function Note(props){
-    return <div className="note">
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
+  );
 }
 
 export default Note;
