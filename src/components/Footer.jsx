53edf08b8dcfd72fa85b45ecd77add5739c44e15
Asmita Pathare <asmitapat9@gmail.com>
2020-06-09 10:18:11 +0530

    Functionalities such as Add and Delete Note included

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     const currentYear= new Date().getFullYear();
     return <footer><p>
-        Copyright © {currentYear}
+        Copyright © {currentYear} asmitapathare
     </p></footer>
 }
 
